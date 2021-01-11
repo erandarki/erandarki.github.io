2345133f232d101e78be32b974a14846984aa717
erandarki <erandarki@yahoo.com>
2021-01-11 22:11:08 +0200

    Update parallax.js

--- js/parallax.js
+++ js/parallax.js
@@ -1,8 +1,11 @@
-$(document).ready(function(){
+$(document).ready(function () {
+
+  let mediaQuery = window.matchMedia('(min-width: 767px)');
 
   // Populate images from data attributes.
-  var scrolled = $(window).scrollTop()
-  $('.parallax').each(function(index) {
+  var scrolled = $(window).scrollTop();
+  if (mediaQuery.matches) {
+    $('.parallax').each(function (index) {
       var imageHeight = $(this).data('height')
       $(this).css('height', imageHeight)
 
@@ -11,43 +14,45 @@ $(document).ready(function(){
       var height = $(this).height()
       var diff = scrolled - initY
       var ratio = Math.round((diff / height) * 100)
-      $(this).css('background-position','center ' + parseInt(-(ratio * 1.5)) + 'px')
-  })
-
-  // Attach scroll event to window. Calculate the scroll ratio of each element
-  // and change the image position with that ratio.
-  // https://codepen.io/lemagus/pen/RWxEYz
-  $(window).scroll(function() {
-    var scrolled = $(window).scrollTop()
-    $('.parallax').each(function(index, element) {
-      var initY = $(this).offset().top
-      var height = $(this).height()
-      var endY  = initY + $(this).height()
-
-      // Check if the element is in the viewport.
-      var visible = isInViewport(this)
-      if(visible) {
-        var diff = scrolled - initY
-        var ratio = Math.round((diff / height) * 100)
-        $(this).css('background-position','center ' + parseInt(-(ratio * 1.5)) + 'px')
-      }
+      $(this).css('background-position', 'center ' + parseInt(-(ratio * 1.5)) + 'px')
     })
-  })
-})
 
-// Check if the element is in the viewport.
-// http://www.hnldesign.nl/work/code/check-if-element-is-visible/
-function isInViewport(node) {
-  // Am I visible? Height and Width are not explicitly necessary in visibility
-  // detection, the bottom, right, top and left are the essential checks. If an
-  // image is 0x0, it is technically not visible, so it should not be marked as
-  // such. That is why either width or height have to be > 0.
-  var rect = node.getBoundingClientRect()
-  return (
-    (rect.height > 0 || rect.width > 0) &&
-    rect.bottom >= 0 &&
-    rect.right >= 0 &&
-    rect.top <= (window.innerHeight || document.documentElement.clientHeight) &&
-    rect.left <= (window.innerWidth || document.documentElement.clientWidth)
-  )
-}
+    // Attach scroll event to window. Calculate the scroll ratio of each element
+    // and change the image position with that ratio.
+    // https://codepen.io/lemagus/pen/RWxEYz
+    $(window).scroll(function () {
+      var scrolled = $(window).scrollTop()
+      $('.parallax').each(function (index, element) {
+        var initY = $(this).offset().top
+        var height = $(this).height()
+        var endY = initY + $(this).height()
+
+        // Check if the element is in the viewport.
+        var visible = isInViewport(this)
+        if (visible) {
+          var diff = scrolled - initY
+          var ratio = Math.round((diff / height) * 100)
+          $(this).css('background-position', 'center ' + parseInt(-(ratio * 1.5)) + 'px')
+        }
+      })
+    })
+
+    // Check if the element is in the viewport.
+    // http://www.hnldesign.nl/work/code/check-if-element-is-visible/
+    function isInViewport(node) {
+      // Am I visible? Height and Width are not explicitly necessary in visibility
+      // detection, the bottom, right, top and left are the essential checks. If an
+      // image is 0x0, it is technically not visible, so it should not be marked as
+      // such. That is why either width or height have to be > 0.
+      var rect = node.getBoundingClientRect()
+      return (
+        (rect.height > 0 || rect.width > 0) &&
+        rect.bottom >= 0 &&
+        rect.right >= 0 &&
+        rect.top <= (window.innerHeight || document.documentElement.clientHeight) &&
+        rect.left <= (window.innerWidth || document.documentElement.clientWidth)
+      )
+    }
+  }
+
+});
