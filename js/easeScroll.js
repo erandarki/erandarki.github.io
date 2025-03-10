85034d3525196deb3406863039d525a167b3b911
Eran Darki <erandarki@yahoo.com>
2025-03-10 21:35:55 +0200

    Update easeScroll.js

--- js/easeScroll.js
+++ js/easeScroll.js
@@ -178,11 +178,11 @@ $.fn.easeScroll = function(options) {
 
         var settings = $.extend({
             // Settings
-            frameRate: 5, // frames per second
+            frameRate: 3, // frames per second
             
             // Scrolling Core
             animationTime: 1500, // [ms]
-            stepSize: 10, // [px]
+            stepSize: 6, // [px]
 
             // Pulse (less tweakable)
             // ratio of "tail" to "acceleration"
@@ -191,7 +191,7 @@ $.fn.easeScroll = function(options) {
             pulseNormalize: 1,
 
             // Acceleration
-            accelerationDelta: 20, // 50
+            accelerationDelta: 1, // 50
             accelerationMax: 1, // 3
 
             // Keyboard
