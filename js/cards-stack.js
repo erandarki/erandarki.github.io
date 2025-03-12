696867c17497757b3a5d81fbecb0c8888a7cdff8
Eran Darki <erandarki@yahoo.com>
2025-03-12 10:50:40 +0200

    Update cards-stack.js

--- js/cards-stack.js
+++ js/cards-stack.js
@@ -3,7 +3,10 @@
 
   const cardBlocks = document.querySelectorAll(".stack-cards");
 
-  cardBlocks.forEach((cardBlock) => {Array.from(cardBlock.children).forEach(
+  cardBlocks.forEach((cardBlock) => {
+    cardBlock.style.paddingBottom = `${Math.max(cardBlock.children.length, 1)}em`;
+
+    Array.from(cardBlock.children).forEach(
       (card, i) => (card.style.transform = `translateY(${i}em)`)
     );
   });
