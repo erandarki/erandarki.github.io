f63609c22a87b038cd8fb638d4d526a5e2e99d23
Eran Darki <erandarki@yahoo.com>
2025-05-03 14:55:13 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -4,7 +4,7 @@ $(document).ready(function () {
     $('.invisible').removeClass('invisible');
     $('.frame').addClass('show-frame');
     $('nav').addClass('nav-show');
-  }, 2000);
+  }, 1400);
 
   // On page scroll execute scrollIndicator
   window.onscroll = function() {scrollIndicator()};
@@ -21,13 +21,13 @@ $(document).ready(function () {
 
   // Hello animation
   tlHello = new TimelineLite({ paused: true });
-  tlHello.staggerFrom($("#hello_h > *"), 0.6, { drawSVG: "0%", ease: Power3.easeOut }, 0.2, 0.8);
+  tlHello.staggerFrom($("#hello_h > *"), 0.6, { drawSVG: "0%", ease: Power3.easeOut }, 0.2, 0);
   tlHello.staggerFrom($("#hello_e > *"), 0.6, { drawSVG: "0%", ease: Power3.easeOut }, 0.2, "-=0.7");
   tlHello.staggerFrom($("#hello_l1 > *"), 0.6, { drawSVG: "0%", ease: Power3.easeOut }, 0.4, "-=1.2");
   tlHello.staggerFrom($("#hello_l2 > *"), 0.6, { drawSVG: "0%", ease: Power3.easeOut }, 0.4, "-=1");
   tlHello.staggerFrom($("#hello_o > *"), 1.2, { drawSVG: "0%", ease: Power3.easeOut }, 0.2, "-=1.2");
   tlHello.staggerFrom($("#hello_dot > *"), 0.6, { scale: 0, transformOrigin: "50% 50%", ease: Power3.easeOut }, 0.2, "-=0.8");
-  tlHello.staggerFrom($(".scroll-down"), 1.2, { bottom: -70, ease: Power3.easeOut }, 1.2, 2.2);
+  tlHello.staggerFrom($(".scroll-down"), 1.2, { bottom: -70, ease: Power3.easeOut }, 1.2, 1.4);
 
   tlHello.play().timeScale(1);
 
